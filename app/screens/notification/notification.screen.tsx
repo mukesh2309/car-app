e60caa8fff3510a72f3d721fca316de64e25671c
mukesh2309 <shelkemukesh6@gmail.com>
2025-04-24 01:01:20 +0530

    udpate

--- app/screens/notification/notification.screen.tsx
+++ app/screens/notification/notification.screen.tsx
@@ -1,10 +1,22 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {FlatList, View} from 'react-native';
+import HeaderComponent from '../../components/header/component';
+import {createStyles} from './notifcation.styles';
+import SingleItem from './component/singleItem';
+import {renderMarginBottom} from '../../utils/ui-utils';
 
 const NotificationScreen = () => {
+  const styles = createStyles();
   return (
-    <View>
-      <Text>NotificationScreen</Text>
+    <View style={styles.container}>
+      <HeaderComponent title="Notification" hasBack />
+      <View style={styles.main}>
+        <FlatList
+          data={[1, 2, 3, 3, 3, 4, 3]}
+          renderItem={({item}) => <SingleItem unRead={item % 2 === 0} />}
+        />
+        {renderMarginBottom(80)}
+      </View>
     </View>
   );
 };
